c35c917566de9b3cc5ece1ea0236299bbec6c668
JustOff <Off.Just.Off@gmail.com>
2020-07-24 18:08:19 +0300

    Setup auto update

---
+++ release.cmd
@@ -0,0 +1,13 @@
+@echo off
+set VER=1.0.1
+
+sed -i -b -E "s/\"version\": \".+?\"/\"version\": \"%VER%\"/" package.json
+sed -i -b -E "s/version>.+?</version>%VER%</; s/download\/.+?\/moonscribe-.+?\.xpi/download\/%VER%\/moonscribe-%VER%\.xpi/" update.xml
+
+del bootstrap.js install.rdf
+if exist moonscribe.xpi del moonscribe.xpi
+call jpm xpi
+
+7z x moonscribe.xpi bootstrap.js install.rdf
+if exist moonscribe-%VER%.xpi del moonscribe-%VER%.xpi
+ren moonscribe.xpi moonscribe-%VER%.xpi

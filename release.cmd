b918991a29e634d7af4b2426a4dba4b48711c41d
JustOff <Off.Just.Off@gmail.com>
2020-07-29 22:12:24 +0300

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=1.0.2
+set VER=1.0.3
 
 sed -i -b -E "s/\"version\": \".+?\"/\"version\": \"%VER%\"/" package.json
 sed -i -b -E "s/version>.+?</version>%VER%</; s/download\/.+?\/moonscribe-.+?\.xpi/download\/%VER%\/moonscribe-%VER%\.xpi/" update.xml
